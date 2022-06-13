e00f8265c07a299c7a95d6158c4bbd0cc514ee16
Danilo Murilo <daruloto128@gmail.com>
2022-06-13 02:34:25 -0300

    upload JPA project

--- aulajpa/src/main/java/serasinha/model/Wallet.java
+++ aulajpa/src/main/java/serasinha/model/Wallet.java
@@ -1,15 +1,39 @@
-//package serasinha.model;
-//
-//import java.io.Serializable;
-//import javax.persistence.*;
-//
-///**
-//*
-//* @author millenacosta
-//*/
-//@Entity
-//public class Wallet implements Serializable{
-//
-//	
-//
-//}
+package serasinha.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Wallet {
+    @Id @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    @Column(nullable=false)
+    private Long account_id;
+    @Column(nullable=false)
+    private Float amount;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+	public Wallet(Long account_id, Float amount) {
+		super();
+		this.account_id = account_id;
+		this.amount = amount;
+	}
+	
+	public void createWallet() {
+		Wallet wallet = new Wallet(null, 0F);
+    	
+    	EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "serasinhadb" );
+    	EntityManager entityManager = emfactory.createEntityManager();
+    	entityManager.getTransaction().begin();
+	   	entityManager.persist(wallet);
+	   	entityManager.getTransaction().commit();
+	}
+    
+    
+}
