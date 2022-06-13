7450cbbbf696631476b4262afe23e659a9ee2089
Danilo Murilo <daruloto128@gmail.com>
2022-06-13 17:47:25 -0300

    upload JPA project

--- aulajpa/src/main/java/serasinha/aplicacao/Main.java
+++ aulajpa/src/main/java/serasinha/aplicacao/Main.java
@@ -7,8 +7,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		Bank_Card card = new Bank_Card(null,null, null, null, null, null);
-		card.registerCreditCard(null, null, "123", 123, "13/06/2022", "001", "00000000");
+//		Bank_Card card = new Bank_Card(null,null, null, null, null, null);
+//		card.registerCreditCard(null, null, "123", 123, "13/06/2022", "001", "00000000");
+//		
+		Transaction transaction = new Transaction(null, null, null, null);
+		transaction.createTransaction(1l, 2l, 50f, "13/06/2022");
 		
 		System.out.println("Cadastrado com sucesso!");
 		
---
+++ aulajpa/src/main/java/serasinha/service/TransactionService.java
@@ -0,0 +1,28 @@
+package serasinha.service;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.persistence.*;
+import javax.ws.rs.*;
+import serasinha.model.*;
+
+@Path("transaction")
+public class TransactionService {
+	
+	@POST
+	@Consumes
+	public String createTransaction(
+            @FormParam("buyer_account_id") Long buyer_account_id, 
+            @FormParam("seller_account_id") Long seller_account_id, 
+            @FormParam("value") Float value
+    ) {
+		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+		String data = formato.format(new Date());
+		
+		Transaction transaction = new Transaction(null, null, null, null);
+		transaction.createTransaction(buyer_account_id, seller_account_id, value, data);
+
+		return "Transacao feita com sucesso!";
+	}
+}
